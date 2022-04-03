a1dd4b44865e11569edd6825abfefea6436d0a69
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-04-03 23:22:24 +0900

    Fix: FIx tick sound is played twice

--- src/engine/data/scripts/slide-tick.ts
+++ src/engine/data/scripts/slide-tick.ts
@@ -138,9 +138,12 @@ export function slideTick(isCritical: boolean, isVisible = true): Script {
                     0,
                     'tick'
                 ),
-                isCritical
-                    ? playCriticalTickJudgmentSFX()
-                    : playTickJudgmentSFX(),
+                Or(
+                    options.isAutoplay,
+                    isCritical
+                        ? playCriticalTickJudgmentSFX()
+                        : playTickJudgmentSFX()
+                ),
             ]
         )
     }
