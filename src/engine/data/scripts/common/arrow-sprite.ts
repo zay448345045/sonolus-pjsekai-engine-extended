72fa1ce2c1d55a961afc2075382d9fe263c93601
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-07-10 06:42:14 +0900

    Change: フリックの高さを変更

--- src/engine/data/scripts/common/arrow-sprite.ts
+++ src/engine/data/scripts/common/arrow-sprite.ts
@@ -173,7 +173,7 @@ export class ArrowSprite {
         const [sprite, x1, y1, x2, y2, x3, y3, x4, y4] = layout
         const yShift = Add(
             Multiply(
-                Subtract(Multiply(this.animateProgress, 1.5), 0.5),
+                Subtract(Multiply(this.animateProgress, 1.2), 0.2),
                 scale,
                 0.2
             )
